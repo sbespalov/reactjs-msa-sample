0f48539533eda8a5fc64e39b3bd6ec4120678826
sbespalov <sbespalov@wellink.ru>
2016-10-13 11:02:15 +0600

    Side Bar Menu initial commit

--- basel-web-portal-client/src/components/BaselMainPage.jsx
+++ basel-web-portal-client/src/components/BaselMainPage.jsx
@@ -1,48 +1,38 @@
 import React from 'react';
 import styles from './app.css';
-import {Accordion, Panel, Table, ButtonGroup, Button, MenuItem, Nav, NavItem} from 'react-bootstrap';
+import {Accordion, Panel, Table, ButtonGroup, Button, MenuItem, Nav, NavItem, Navbar, NavDropdown, Glyphicon} from 'react-bootstrap';
 
 export default React.createClass( {
 
     render: function() {
         return <div className={styles.bslPageContainer}>
             <div className={styles.bslSideBarMenuContainer}>
-                <Accordion>
-                    <Panel header={<Button bsStyle="link">Position</Button>} eventKey="1">
-                        blabla
-                    </Panel>
-                    <Panel header={<NavItem eventKey={1} title="Item">NavItem 1 content</NavItem>} eventKey="2">
-                        <Table responsive>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <MenuItem>link</MenuItem>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <MenuItem>link</MenuItem>
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <MenuItem>link</MenuItem>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </Table>
-                        <Nav bsStyle="pills" stacked activeKey={1}>
-                            <NavItem eventKey={1} title="Item">NavItem 1 content</NavItem>
-                            <NavItem eventKey={2} title="Item">NavItem 2 content</NavItem>
-                            <NavItem eventKey={3} title="Item">NavItem 3 content</NavItem>
+                <Navbar fluid className={styles.sidebar}>
+
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="#">Ф.Киркоров </a>
+                        </Navbar.Brand>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+
+                    <Navbar.Collapse>
+                        <Navbar.Text className={styles.bslUserMenu}>
+                            <Navbar.Link href="#"><Glyphicon glyph="home"/></Navbar.Link>
+                            <Navbar.Link href="#"><Glyphicon glyph="log-out"/></Navbar.Link>
+                        </Navbar.Text>
+                        <Nav>
+                            <NavDropdown eventKey={1} title="Monitoring" id="basic-nav-dropdown" animation={true}>
+                                <MenuItem eventKey={1.1}>Action</MenuItem>
+                            </NavDropdown>
+                            <NavItem eventKey={2}>Reports</NavItem>
+                            <NavItem eventKey={3}>References</NavItem>
                         </Nav>
-                    </Panel>
-                    <Panel header="Cource" eventKey="3">
-                        blabla
-                    </Panel>
-                </Accordion>
+                    </Navbar.Collapse>
+
+                </Navbar>
             </div>
-            <div className={styles.bslPageContent}>bsl-page-content</div>
+            <div className={styles.bslPageContent + ' ' + styles.main}>bsl-page-content</div>
         </div>;
     }
 
