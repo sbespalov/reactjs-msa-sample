19e13df89d9056110ce967094d3956a83105f08b
sbespalov <sbespalov@wellink.ru>
2016-10-13 14:52:22 +0600

    .gitignore

---
+++ basel-web-portal-client/src/components/monitoring/MonitoringPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {PageHeader} from 'react-bootstrap';
+
+
+export default React.createClass( {
+
+    render: function() {
+        return <PageHeader>Monitoring</PageHeader>;
+    }
+
+});
