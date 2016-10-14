bfe9c0b9c04fddb75b3f2dc042bb72e397dcb754
sbespalov <sbespalov@wellink.ru>
2016-10-14 07:11:01 +0600

    Боковое меню вынесено в отдельный компонент

--- basel-web-portal-client/src/components/App.jsx
+++ basel-web-portal-client/src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './app.css';
 import {Nav, NavItem, Navbar, NavDropdown, MenuItem, Glyphicon} from 'react-bootstrap';
+import SideBarMenu from './navigation/SideBarMenu';
 import { hashHistory } from 'react-router'
 
 export default React.createClass( {
@@ -24,32 +25,7 @@ export default React.createClass( {
         var componentInstance = this;
 
         return <div className={styles.bslPageContainer}>
-            <div className={styles.bslSideBarMenuContainer}>
-                <Navbar fluid className={styles.sidebar}>
-
-                    <Navbar.Header>
-                        <Navbar.Brand>
-                            <a href="/">Ф.Киркоров </a>
-                        </Navbar.Brand>
-                        <Navbar.Toggle />
-                    </Navbar.Header>
-
-                    <Navbar.Collapse>
-                        <Navbar.Text className={styles.bslUserMenu}>
-                            <Navbar.Link href="#/"><Glyphicon glyph="home"/></Navbar.Link>
-                            <Navbar.Link href="#/logout"><Glyphicon glyph="log-out"/></Navbar.Link>
-                        </Navbar.Text>
-                        <Nav>
-                            <NavDropdown eventKey={1} title="Monitoring" id="basic-nav-dropdown" ref={( c ) => { c ? componentInstance.addOnClickNavigation( c, '/monitoring' ) : {}; } }>
-                                <MenuItem eventKey={1.1} href="#/security-recalculate">Security recalculate</MenuItem>
-                            </NavDropdown>
-                            <NavItem eventKey={2}>Reports</NavItem>
-                            <NavItem eventKey={3}>References</NavItem>
-                        </Nav>
-                    </Navbar.Collapse>
-
-                </Navbar>
-            </div>
+            <SideBarMenu/>
             <div className={styles.bslPageContent + ' ' + styles.main}>{this.props.children}</div>
         </div>;
     }
---
+++ basel-web-portal-client/src/components/navigation/SideBarMenu.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './sideBarMenu.css';
+import {Nav, NavItem, Navbar, NavDropdown, MenuItem, Glyphicon} from 'react-bootstrap';
+import { hashHistory } from 'react-router'
+
+export default React.createClass( {
+
+    addOnClickNavigation: function( component, route ) {
+        if ( component.onClickNavigation ) {
+            return;
+        }
+        component.onClickNavigation = () => {
+            hashHistory.push( route );
+        }
+        var element = ReactDOM.findDOMNode( component );
+        element.addEventListener( 'click', ( evt ) => {
+            component.onClickNavigation();
+        }, false );
+    },
+
+    render: function() {
+
+        var componentInstance = this;
+
+        return <div id="sidebar-menu" className={styles.bslSideBarMenuContainer}>
+            <Navbar fluid className={styles.sidebar} inverse >
+
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="/">Ф.Киркоров </a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+
+                <Navbar.Collapse>
+                    <Navbar.Text className={styles.bslUserMenu}>
+                        <Navbar.Link href="#/"><Glyphicon glyph="home"/></Navbar.Link>
+                        <Navbar.Link href="#/logout"><Glyphicon glyph="log-out"/></Navbar.Link>
+                    </Navbar.Text>
+                    <Nav>
+                        <NavDropdown eventKey={1} title="Monitoring" id="basic-nav-dropdown" ref={( targetComponent ) => { targetComponent ? componentInstance.addOnClickNavigation( targetComponent, '/monitoring' ) : {}; } }>
+                            <MenuItem eventKey={1.1} href="#/security-recalculate">Security recalculate</MenuItem>
+                        </NavDropdown>
+                        <NavItem eventKey={2}>Reports</NavItem>
+                        <NavItem eventKey={3}>References</NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+
+            </Navbar>
+        </div>;
+    }
+});
