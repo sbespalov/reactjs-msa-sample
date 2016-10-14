b7014820723d56fd24c7bf453789ee33090f6436
sbespalov <serge.bespalov@gmail.com>
2016-10-14 10:10:53 +0600

    Боковое меню вынесено в отдельный компонент

--- basel-web-portal-client/src/components/App.jsx
+++ basel-web-portal-client/src/components/App.jsx
@@ -1,24 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from './app.css';
-import {Nav, NavItem, Navbar, NavDropdown, MenuItem, Glyphicon} from 'react-bootstrap';
 import SideBarMenu from './navigation/SideBarMenu';
-import { hashHistory } from 'react-router'
 
 export default React.createClass( {
-
-    addOnClickNavigation: function( component, route ) {
-        if (component.onClickNavigation){
-            return;
-        }
-        component.onClickNavigation = () => {
-            hashHistory.push( route );
-        }
-        var element = ReactDOM.findDOMNode( component );
-        element.addEventListener( 'click', ( evt ) => {
-            component.onClickNavigation();
-        }, false );
-    },
 
     render: function() {
 
