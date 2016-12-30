31244eebbd4f397e55015fb5b75c8601ba3fe71a
sbespalov <serge.bespalov@gmail.com>
2016-12-30 07:41:15 +0600

    BASELIMP-1677: изменения структуры модулей

--- basel-service-brd/src/main/java/ru/sbrf/basel/service/brd/controller/BookServiceController.java
+++ basel-service-brd/src/main/java/ru/sbrf/basel/service/brd/controller/BookServiceController.java
@@ -2,19 +2,19 @@ package ru.sbrf.basel.service.brd.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.common.base.Joiner;
 
+import ru.sbrf.basel.service.api.common.dto.FindPageResponseDto;
 import ru.sbrf.basel.service.brd.api.BookServiceApi;
 import ru.sbrf.basel.service.brd.api.dto.BslBook;
 import ru.sbrf.basel.service.brd.api.dto.BslFindBookListRequest;
 import ru.sbrf.basel.service.brd.api.dto.BslFindBookListResponse;
 import ru.sbrf.basel.service.brd.domain.Book;
 import ru.sbrf.basel.service.brd.repository.BookRepository;
-import ru.sbrf.basel.service.generic.api.BslFindPageResponseDto;
-import ru.sbrf.basel.service.generic.domain.BslPagable;
 
 @RestController
 public class BookServiceController implements BookServiceApi
@@ -38,7 +38,10 @@ public class BookServiceController implements BookServiceApi
                                                       Joiner.on("")
                                                             .skipNulls()
                                                             .join(bslFindBookListRequest.getSourceSystem(), "%"),
-                                                      new BslPagable(bslFindBookListRequest.getPageRequest()));
+                                                      new PageRequest(
+                                                              bslFindBookListRequest.getPageRequest().getPageNumber() -
+                                                                      1,
+                                                              bslFindBookListRequest.getPageRequest().getPageSize()));
         Page<BslBook> bslBookPage = bookPage.map((book) -> {
             BslBook item = new BslBook();
             item.setBookCode(book.getBookCode());
@@ -47,7 +50,8 @@ public class BookServiceController implements BookServiceApi
 
             return item;
         });
-        result.setPageResponse(new BslFindPageResponseDto<>(bookPage));
+        result.setPageResponse(new FindPageResponseDto(bslBookPage.getNumber() + 1, bslBookPage.getSize(),
+                Long.valueOf(bslBookPage.getNumberOfElements())));
         result.setBookList(bslBookPage.getContent());
         return result;
     }
